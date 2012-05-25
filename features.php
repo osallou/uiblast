786178ef983b6db8cacca7e76f0daa133be5746f
Olivier Sallou <olivier.sallou@irisa.fr>
2012-05-25 13:19:05 +0200

    fix query

--- features.php
+++ features.php
@@ -2,7 +2,8 @@
 
  header('Content-type: application/json');
  
- $query = "chr:".$_GET["id"]." AND (start:[".$_GET["to"]." TO *] OR end:[* TO ".$_GET["from"]."])";
+// $query = "chr:".$_GET["id"]." AND (start:[".$_GET["to"]." TO *] OR end:[* TO ".$_GET["from"]."])";
+  $query = "chr:".$_GET["id"]." AND (start:[".$_GET["from"]." TO ".$_GET["to"]."] OR end:[".$_GET["from"]." TO ".$_GET["to"]."] OR ( start:[* TO ".$_GET["from"]."] AND end:[".$_GET["to"]." TO *]))";
  
  print file_get_contents('http://seqcrawler.genouest.org/solr/select?rows=200&wt=json&q='.urlencode($query));
 
